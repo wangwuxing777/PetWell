1323d7c98ca4fe78934dfd8107dcfcbafd37988d
Wang Wuxing <w18090118927@163.com>
2025-12-24 13:42:51 +0800

    Initial Commit

---
+++ PetWell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetWell
+//
+//  Created by 王武行 on 2025/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetWell/PetWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetWellApp.swift
+//  PetWell
+//
+//  Created by 王武行 on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PetWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
